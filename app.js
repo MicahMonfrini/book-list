4061a122752d9aac6cd7de5fb99a905e191cf652
Micah Monfrini <ffrock905@gmail.com>
2019-01-24 22:49:41 -0600

    add form event listener

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-// Book Constructor
+// CONSTRUCTORS
+
+// book
+
 function Book(title, author, isbn) {
   this.title = title;
   this.author = author;
   this.isbn = isbn;
 }
 
-// UI Constructor
-function UI() {
-  
-}
+// UI
+function UI() {}
+
+// EVENT LISTENERS
+document.getElementById('book-form').addEventListener('submit', (e) => {
+  console.log('test');
+
+  e.preventDefault();
+})
