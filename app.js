c539c98f4fea1ab19cc185116017d5a0e9cee022
Micah Monfrini <ffrock905@gmail.com>
2019-01-24 23:02:23 -0600

    fix delete

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ UI.prototype.addBookToList = function(book) {
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.isbn}</td>
-    <td><a href="#" class="delete>X</td>
+    <td><a href="#" class="delete">X<a></td>
   `
 
   // append to list
