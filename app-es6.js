2b061deca251e021bb805f01d191a1fbb6172755
Micah Monfrini <ffrock905@gmail.com>
2019-01-25 21:30:26 -0600

    copy logic into new prototypes

--- app-es6.js
+++ app-es6.js
@@ -1,3 +1,5 @@
+// CLASSES
+
 class Book {
   constructor(title, author, isbn) {
     this.title = title;
@@ -7,16 +9,50 @@ class Book {
 }
 
 class UI {
-  addBookToList() {
-
+  addBookToList(book) {
+    // book list variable
+  const list = document.getElementById('book-list');
+  // create tr element
+  const row = document.createElement('tr');
+  // insert table cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X<a></td>
+  `
+  // append to list
+  list.appendChild(row);
   }
-  deleteBook() {
 
+  deleteBook(target) {
+    if(target.className === 'delete') {
+      target.parentElement.parentElement.remove();
+    }
   }
-  showAlert() {
 
+  showAlert(message, className) {
+    // create div
+    const div = document.createElement('div');
+    // add classes
+    div.className = `alert ${className}`;
+    // add text
+    div.appendChild(document.createTextNode(message));
+    // get parent
+    const container = document.querySelector('.container');
+    // get form
+    const form = document.querySelector('#book-form');
+    // insert alert
+    container.insertBefore(div, form);
+
+    // timeout after 3 sec
+    setTimeout(() => {
+      document.querySelector('.alert').remove();
+    }, 3000)
   }
   clearFields() {
-    
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
   }
 }
